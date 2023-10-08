e05c6c36f3f241f099887b79c8efd7db400cb348
Nguyễn Đức Thắng <thang.ndu.63cntt@ntu.edu.vn>
2023-10-08 22:22:51 +0700

    Update Layout ExNo3

--- exno3/app/src/main/java/com/ndthang/exno3/MainActivity.java
+++ exno3/app/src/main/java/com/ndthang/exno3/MainActivity.java
@@ -21,51 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editTextNumber1 = findViewById(R.id.editTextNumber1);
-        editTextNumber2 = findViewById(R.id.editTextNumber2);
-
-        textResul = findViewById(R.id.textResul);
-        btnAdd = findViewById(R.id.btnAdd);
-        btnMinus = findViewById(R.id.btnMinus);
-        btnMulti = findViewById(R.id.btnMulti);
-        btnDivide = findViewById(R.id.btnDivide);
-
-        String text1 = editTextNumber1.getText().toString();
-        String text2 = editTextNumber2.getText().toString();
-
-        float num1 = Float.parseFloat(text1);
-        float num2 = Float.parseFloat(text2);
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                result = num1 + num2;
-                textResul.setText("" + result);
-            }
-        });
-        btnMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                result = num1 - num2;
-                textResul.setText("" + result);
-            }
-        });
-        btnMulti.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                result = num1 * num2;
-                textResul.setText("" + result);
-            }
-        });
-        btnDivide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (num2 == 0) textResul.setText("Số chia không thể = 0".toUpperCase());
-                result = num1 / num2;
-                textResul.setText("" + result);
-            }
-        });
     }
     void setBtnAdd(){
 
