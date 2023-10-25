3f10caf4c204be35286888b51036d405083634aa
Nguyễn Đức Thắng <thang.ndu.63cntt@ntu.edu.vn>
2023-10-25 16:48:06 +0700

    Update vài thứ

---
+++ CustomizeListView/app/src/main/java/com/ndthang/customizelistview/DetailActivity.java
@@ -0,0 +1,34 @@
+package com.ndthang.customizelistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.ndthang.customizelistview.databinding.ActivityDetailBinding;
+import com.ndthang.customizelistview.databinding.ActivityMainBinding;
+
+public class DetailActivity extends AppCompatActivity {
+    ActivityDetailBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = this.getIntent();
+        if (intent != null){
+            String name = intent.getStringExtra("name");
+            String time = intent.getStringExtra("time");
+            int ingredients = intent.getIntExtra("ingredients", R.string.maggiIngredients);
+            int desc = intent.getIntExtra("desc", R.string.maggieDesc);
+            int image = intent.getIntExtra("image", R.mipmap.maggi);
+            binding.detailName.setText(name);
+            binding.detailTime.setText(time);
+            binding.detailDesc.setText(desc);
+            binding.detailIngredients.setText(ingredients);
+            binding.detailImage.setImageResource(image);
+        }
+    }
+}
---
+++ CustomizeListView/app/src/main/java/com/ndthang/customizelistview/ListData.java
@@ -0,0 +1,15 @@
+package com.ndthang.customizelistview;
+
+public class ListData {
+    String name, time;
+    int ingredients, desc;
+    int image;
+
+    public ListData(String name, String time, int ingredients, int desc, int image) {
+        this.name = name;
+        this.time = time;
+        this.ingredients = ingredients;
+        this.desc = desc;
+        this.image = image;
+    }
+}
